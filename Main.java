69e568d70df1cf9649539ec29ab0e5c3883de7cb
Charles Wilmer <charleswilmer@zipcoders-MacBook-Pro.local>
2019-02-08 17:32:01 -0500

    sum of input completed

--- Main.java
+++ Main.java
@@ -1,10 +1,21 @@
 /**
  * Created by iyasuwatts on 10/17/17.
  */
+import java.util.Scanner; 
 public class Main {
 
     public static void main(String[] args){
-        // comment to show commit
+        Scanner in = new Scanner(System.in);
+       int userN ; 
+       int sum = 0;
+       
+       System.out.println("Input a number");
+        userN = in.nextInt();
+       for( int i = 0; i <= userN; i++){
+           sum += i;
+        } 
         
+       
+       System.out.println("the sum is " + sum);
     }
 }
